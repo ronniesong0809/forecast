fe363cd96de7126507a2d2c8ab7a91acaa0063f3
Ronnie Song <ronniesong0809@gmail.com>
2021-02-15 00:41:25 -0800

    add user-agent and change formate

--- wttr.sh
+++ wttr.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl wttr.in/{Portland,Shanghai}?format=v2
+set -eux
+
+USERAGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+
+curl -H "User-Agent: $USERAGENT" wttr.in/{Portland,Shanghai}?format=4
