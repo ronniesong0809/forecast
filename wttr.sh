b890116fe5d0876a21bdfb21559eabb2233ea4ea
Ronnie Song <ronniesong0809@gmail.com>
2021-02-15 03:24:47 -0800

    twists email body and set to daily alert

--- wttr.sh
+++ wttr.sh
@@ -3,4 +3,4 @@ set -eux
 
 USERAGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
 
-curl -H "User-Agent: $USERAGENT" -o weather.html wttr.in/{Portland,Shanghai}?format="%m+%l:+%c+%C+%p,+🌡️%t+(%f),+%h,+🌬️%w,+%P\nDawn:+%D,+Sunrise:+%S,+Zenith:+%z,+Sunset:+%s,+Dusk:+%d\n"
+curl -H "User-Agent: $USERAGENT" -o weather.html wttr.in/Portland=?format="%m+%l:<br/>%c+%C+/%p<br/>🌡️%t+(%f),+%💧h,+🌬️%w,+%P\n<br/>Dawn:+%D,+Sunrise:+%S,+Zenith:+%z,+Sunset:+%s,+Dusk:+%d\n"
