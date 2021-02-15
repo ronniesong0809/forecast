e12cc4447516e9c01881bc58e092765a20a3cb9f
Ronnie Song <ronniesong0809@gmail.com>
2021-02-15 01:05:09 -0800

    twists format

--- wttr.sh
+++ wttr.sh
@@ -3,4 +3,4 @@ set -eux
 
 USERAGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
 
-curl -H "User-Agent: $USERAGENT" wttr.in/{Portland,Shanghai}?format=4
+curl -H "User-Agent: $USERAGENT" wttr.in/{Portland,Shanghai}?format="%m+%l:+%c+%C+%p,+🌡️%t+(%f),+%h,+🌬️%w,+%P\nDawn:+%D,+Sunrise:+%S,+Zenith:+%z,+Sunset:+%s,+Dusk:+%d\n"
